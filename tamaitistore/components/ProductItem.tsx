c1603c87e7012bf6e9a121173e81d26978a38952
Inzendis <christian-game13@hotmail.com>
2023-05-31 17:26:54 -0400

    Skeleton of HomePage Almost Done

--- tamaitistore/components/ProductItem.tsx
+++ tamaitistore/components/ProductItem.tsx
@@ -8,15 +8,15 @@ interface ProductDetailsProps {
 
 const ProductItem: React.FC<ProductDetailsProps> = ({ product }) => {
   return (
-    <div className="card justify-center flex flex-col items-center">
+    <div className="justify-center flex flex-col items-center pb-2 ">
       <Link href={`/product/${product.slug}`}>
         <img
           src={product.image}
           alt={product.name}
-          className="flex rounded shadow md:w-[500px] h-[350px]"
+          className="flex rounded shadow w-full object-cover h-[200px] md:h-[350px]"
         ></img>
       </Link>
-      <div className="flex flex-col items-center justify-center p-5">
+      <div className="flex flex-col items-center justify-center pt-5">
         <Link href={`/product/${product.slug}`}>
           <h2 className="text-lg">{product.name} </h2>
         </Link>
