6e727032df31fef4090abd8e852cce9dbdfd138e
Inzendis <christian-game13@hotmail.com>
2023-06-05 02:34:53 -0400

    Fixing failed deployment

--- tamaitistore/src/pages/_app.tsx
+++ tamaitistore/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "@/styles/globals.css";
 import "@/styles/layout.css";
 import "@/styles/homePage.css";
 import "@/styles/productCarouselV2.css";
-import "@/styles/GenderCatalogButton.css";
+import "@/styles/genderCatalogButton.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
